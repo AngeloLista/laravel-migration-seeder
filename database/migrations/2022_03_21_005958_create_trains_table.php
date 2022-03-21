517626248fb5e0208fdb967a0bfc42c9075d1bad
AngeloLista <abstractplayer@gmail.com>
2022-03-21 02:45:25 +0100

    changed types values

--- database/migrations/2022_03_21_005958_create_trains_table.php
+++ database/migrations/2022_03_21_005958_create_trains_table.php
@@ -17,13 +17,13 @@ class CreateTrainsTable extends Migration
             $table->id();
             $table->string('company', 100);
             $table->string('departure_station', 100);
-            $table->string('arrival_station', 5);
+            $table->string('arrival_station', 100);
             $table->dateTime('departure_time');
             $table->dateTime('arrival_time');
             $table->char('code_number', 6)->unique();
             $table->tinyInteger('carriages')->unsigned();
-            $table->boolean('is_on_time', 5)->default(1);
-            $table->boolean('is_canceled', 5)->default(0);
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_canceled')->default(0);
             $table->timestamps();
         });
     }
