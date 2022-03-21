f95299aacfa90b55ccbf18d78cdcb603d865441e
AngeloLista <abstractplayer@gmail.com>
2022-03-21 04:10:20 +0100

    fixed seeder

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    // protected $fillable = [
+    //     'company',
+    //     'departure_station',
+    //     'arrival_station',
+    //     'departure_time',
+    //     'arrival_time',
+    //     'code_number',
+    //     'carriages',
+    //     'is_on_time',
+    //     'is_canceled'
+    // ];
 }
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,7 +13,41 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        $trains = config('trains');
+        $trains = [
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Milano',
+                'arrival_station' => 'Monza',
+                'departure_time' => '2022-03-21 12:45:29',
+                'arrival_time' => '2022-03-21 13:05:29',
+                'code_number' => '31319',
+                'carriages' => '15',
+                'is_on_time' => true,
+                'is_canceled' => false
+            ],
+            [
+                'company' => 'Italo',
+                'departure_station' => 'Roma',
+                'arrival_station' => 'Napoli',
+                'departure_time' => '2022-03-23 15:47:16',
+                'arrival_time' => '2022-03-23 18:47:16',
+                'code_number' => '193403',
+                'carriages' => '16',
+                'is_on_time' => true,
+                'is_canceled' => false
+            ],
+            [
+                'company' => 'Italo',
+                'departure_station' => 'Firenze',
+                'arrival_station' => 'Bolzano',
+                'departure_time' => '2022-03-25 13:47:16',
+                'arrival_time' => '2022-03-25 19:47:16',
+                'code_number' => '159047',
+                'carriages' => '16',
+                'is_on_time' => true,
+                'is_canceled' => false
+            ]
+        ];
 
         foreach ($trains as $train) {
             $new_train = new Train();
@@ -26,6 +60,7 @@ class TrainsTableSeeder extends Seeder
             $new_train->carriages = $train['carriages'];
             $new_train->is_on_time = $train['is_on_time'];
             $new_train->is_canceled = $train['is_canceled'];
+            $new_train->save();
         }
     }
 }
